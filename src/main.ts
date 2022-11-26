43e51deda148370ddb43bfd914b2290c59865bc3
Marc Brüderlin <marc.bruederlin@exconcept.com>
2022-11-26 11:10:49 +0100

    Add tailwind to the project

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
-import "./assets/main.css";
+import "./assets/style.css";
 
 const app = createApp(App);
 
